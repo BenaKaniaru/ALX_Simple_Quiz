0dfce90c77204e956ec8fa8c7185aa123af560e4
Benard Kaniaru <kaniarubenard623@gmailcom>
2024-10-05 10:08:36 +0300

    Finished the Quiz

---
+++ quiz.js
@@ -0,0 +1,22 @@
+const correctAnswer = '4';
+
+
+
+function checkAnswer () {
+  let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+console.log (userAnswer);
+
+  let textContent = document.getElementById('feedback');
+
+if (userAnswer == correctAnswer) {
+  textContent.innerHTML = "Correct! Well done."
+ } else {
+  textContent.innerHTML = "That's incorrect. Try again!"
+ }
+
+ console.log(textContent)
+};
+
+let button = document.getElementById('submit-answer');
+
+button.addEventListener('click', checkAnswer);
